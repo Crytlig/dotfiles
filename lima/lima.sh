7d02f8cf5546c4c4b2ca59f3a6915ce0bc6b8324
Claes Rytlig <crytlig@hotmail.com>
2026-02-11 18:56:02 +0100

    update lima script

--- lima/lima.sh
+++ lima/lima.sh
@@ -11,24 +11,32 @@ RESET="\033[0m"
 
 
 command=$1
+name=$2
+
+if [[ -z $name ]]; then
+  echo "name not provided as second argument"
+  exit 1
+fi;
 
 
 if [[ "$command" != "create" ]] && [[ "$command" != "delete" ]]; then
   echo "command should be create or delete. Found $command"
-  exit 0
+  exit 1
 fi;
 
 
+
+
 if [[ "$command" == "create" ]]; then
-  echo -e "${YELLOW}creating lima-vm with mise template"
+  echo -e "${YELLOW}creating lima-vm with $2 template"
   sleep 1
-  cat mise.yaml | limactl create --name=mise - && limactl start mise --mount-only .:w
+  cat ./mise.lima.yaml | limactl create --name=$2 - && limactl start $2 --mount-only .:w
 fi
 
 if [[ "$command" == "delete" ]]; then
-  echo -e "${YELLOW}deleting lima-vm with mise template"
+  echo -e "${YELLOW}deleting lima-vm with template"
   sleep 1
-  limactl stop mise -f && limactl delete mise
+  limactl stop $2 -f && limactl delete $2
 fi
 
 # Exec with limactl shell mise
